f7bc957938ffac41d8ffb7b0258876e07d036c46
agnieszkatab <agnieszkamandelt@gmail.com>
2023-03-06 15:58:40 +0100

    zmiana

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,4 +5,6 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-function changeBackgroundColor ()
+function changeBackgroundColor () {
+    
+}
