822342991ea9ebb3c4507b103cd953f8ac73618d
agnieszkatab <agnieszkamandelt@gmail.com>
2023-03-12 10:45:12 +0100

    zmiany na gotowe

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,30 @@
-const firstBtn = document.querySelector(['data-start']);
-const secondBtn = document.querySelector(['data-stop'])
+const firstBtn = document.querySelector('[data-start]');
+const secondBtn = document.querySelector('[data-stop]');
+let changeTime
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
-
-function changeBackgroundColor () {
-    
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBackgroundColor() {
+  changeTime = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000)
+}
+
+
+  firstBtn.addEventListener("click", () => {
+    firstBtn.disabled = true;
+    secondBtn.disabled = false;
+    changeBackgroundColor();
+  });
+
+  secondBtn.addEventListener("click", () => {
+    firstBtn.disabled = false;
+    secondBtn.disabled = true;
+    clearInterval(changeTime);
+  });
+
+
+
